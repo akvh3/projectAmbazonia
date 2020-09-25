38f7ddcdac2435f993527ed578106f1828644e24
sanggwonjang <49293197+sanggwonjang@users.noreply.github.com>
2020-09-25 02:30:36 -0400

    Orphan information carries over to donation page

--- pages/contribute.js
+++ pages/contribute.js
@@ -17,24 +17,17 @@ class Contribute extends Component {
     firestore.collection('orphans')
       .get()
       .then(querySnapshot => {
-        const data = querySnapshot.docs.map(doc => doc.data());
+        const data = querySnapshot.docs.map(doc => {
+          return {id:doc.id, ...doc.data() }
+        });
         this.setState({ orphansList: data })
       });
-      // .then(console.log(this.state['orphansList']));
-    // storage.ref().child().getDownloadURL().then(function(url) {
-    //   console.log(url)
-    // });
   }
 
-  //I dont know if this works.
-  getImage (image) {
-    storage.ref().child('images/' + '${image}.jpg').getDownloadURL().then((url) => {
-      this.setState({images: url})
-    })
-  }
 
   render() {
     const { orphansList } = this.state;
+    console.log(orphansList)
   return (
     <>
     <Head><title>Orphan Connect - Contribute</title></Head>
@@ -50,7 +43,7 @@ class Contribute extends Component {
                         <img class="card-img" src="/placeholde.jpg" alt="Card image cap"></img>
                       </div>
                   <div class="card-body">
-                    <h4 class="card-title">Hi</h4>
+                    <h4 class="card-title">Dummy Person</h4>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget nibh vulputate, porta purus sit amet, sollicitudin magna. Nam quis fringilla lorem. Nulla cursus lectus eget ipsum semper ultricies. Etiam iaculis pulvinar rhoncus. Nullam nulla leo, fringilla mollis ante at, viverra posuere diam. Mauris lobortis lorem at ipsum pellentesque, sed pulvinar diam tempor. In efficitur tincidunt leo, non tincidunt orci vestibulum sagittis. Donec eleifend posuere imperdiet. Praesent maximus vel erat vel sodales. Duis ut sem congue, sodales dui at, commodo mi. Cras gravida sed ex ut egestas.</p>
                     <Link href="/donate"><button type="button" class="button-class i-left">Donate</button></Link>
                     <Link href="/donate"><button type="button" class="button-class i-left">Adoption Plan</button></Link>
@@ -63,9 +56,9 @@ class Contribute extends Component {
                     </div>
                     <div class="card-body">
                       <h4 class="card-title">{orphan.Name}</h4>
-                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget nibh vulputate, porta purus sit amet, sollicitudin magna. Nam quis fringilla lorem. Nulla cursus lectus eget ipsum semper ultricies. Etiam iaculis pulvinar rhoncus. Nullam nulla leo, fringilla mollis ante at, viverra posuere diam. Mauris lobortis lorem at ipsum pellentesque, sed pulvinar diam tempor. In efficitur tincidunt leo, non tincidunt orci vestibulum sagittis. Donec eleifend posuere imperdiet. Praesent maximus vel erat vel sodales. Duis ut sem congue, sodales dui at, commodo mi. Cras gravida sed ex ut egestas.</p>
-                      <Link href="/donate"><button type="button" class="button-class i-left">Donate</button></Link>
-                      <Link href="/donate"><button type="button" class="button-class i-left">Adoption Plan</button></Link>
+                      <p class="card-text">The orphan is {orphan.Age} years old. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget nibh vulputate, porta purus sit amet, sollicitudin magna. Nam quis fringilla lorem. Nulla cursus lectus eget ipsum semper ultricies. Etiam iaculis pulvinar rhoncus. Nullam nulla leo, fringilla mollis ante at, viverra posuere diam. Mauris lobortis lorem at ipsum pellentesque, sed pulvinar diam tempor. In efficitur tincidunt leo, non tincidunt orci vestibulum sagittis. Donec eleifend posuere imperdiet. Praesent maximus vel erat vel sodales. Duis ut sem congue, sodales dui at, commodo mi. Cras gravida sed ex ut egestas.</p>
+                      <Link href="/donate/[id]" as={"/donate/" + orphan.id}><button type="button" class="button-class i-left">Donate</button></Link>
+                      <Link href="/donate/[id]" as={"/donate/" + orphan.id}><button type="button" class="button-class i-left">Adoption Plan</button></Link>
                     </div>
                   </div>
                 ))}
